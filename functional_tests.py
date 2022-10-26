c803b5151878207cb9d7aceda85153c55a7f1969
Igor Dias <igorhenriquedias94@gmail.com>
2022-10-26 19:24:44 -0300

    Utilizando o módulo unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,34 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import unittest
 
+class NewVisitorTest(unittest.TestCase):
 
-options = Options()
-options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-browser = webdriver.Firefox(executable_path=r'C:\Users\igorh\Desktop\Testes_de_software\tdd-project\geckodriver.exe', options=options)
+    def test_can_start_a_list_and_retrieve_it_later(self): 
+    
+        options = Options()
+        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        self.browser = webdriver.Firefox(executable_path=r'C:\Users\igorh\Desktop\Testes_de_software\tdd-project\geckodriver.exe', options=options)
 
-# Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-browser.get('http://localhost:8000')
+    
+        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        self.browser.get('http://localhost:8000')
 
-# Ela nota que o título da página menciona TODO
-assert 'To-Do' in browser.title
+        # Ela nota que o título da página menciona TODO
+        self.assertIn('To-Do', self.browser.title)
 
-# Ela é convidada a entrar com um item TODO imediatamente
+        # Ela é convidada a entrar com um item TODO imediatamente
 
-# Ela digita "Estudar testes funcionais" em uma caixa de texto
+        # Ela digita "Estudar testes funcionais" em uma caixa de texto
 
-# Quando ela aperta enter, a página atualiza, e mostra a lista
-# "1: Estudar testes funcionais" como um item da lista TODO
+        # Quando ela aperta enter, a página atualiza, e mostra a lista
+        # "1: Estudar testes funcionais" como um item da lista TODO
+        
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
 
-# Ainda existe uma caixa de texto convidando para adicionar outro item
-# Ela digita: "Estudar testes de unidade"
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
 
-# A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
 
-# Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-# o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+        # Ela visita a URL: a sua lista TODO ainda está armazenada
 
-# Ela visita a URL: a sua lista TODO ainda está armazenada
+        # Satisfeita, ela vai dormir
+        
+        self.browser.quit()
 
-# Satisfeita, ela vai dormir
-
-browser.quit()
+if __name__ == '__main__':
+    unittest.main()
